3df8fb9d6c87a5387f5f63a04aa569aa4fefa9d0
ruinfoworld <patelrahul9903@gmail.com>
2022-11-22 18:24:36 +0530

    Added user router to reach user specific users api

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,12 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/usertest", (req,res) => {
-    console.log("User Test!!!!")
+    return res.send("In user Test!!!!");
 });
+
+router.post("/usertestPost", (req,res) => {
+    const username = req.body.username;
+    res.send(username);
+})
 
 export default router;
