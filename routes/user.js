156f24fac34fd91ce4594d43f9f39a92bfcfafc7
ruinfoworld <patelrahul9903@gmail.com>
2022-12-02 09:34:52 +0530

    Added auth verify middleware and implemented update user API

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import User from '../models/User.js';
+import { verifyTokenAndAuthentication } from './verifyToken.js';
 
 const router = express.Router();
+
+router.put("/:id", verifyTokenAndAuthentication, async (req,res) => {
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.HASH_KEY
+          ).toString()
+    }
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set : req.body
+        }, {new : true})
+        return res.status(200).json(updatedUser);
+    }catch(err){
+        return res.status(500).json(err)
+    }
+});
 
 export default router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req,res,next) => {
+    const authHeader = req.headers.token;
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+            if(err) return res.status(403).json("Token is invalid!!!")
+            req.user = user;
+            next();
+        })
+    }else{
+        return res.status(403).json("You are not authenticated!!!");
+    }
+}
+
+export const verifyTokenAndAuthentication = (req,res,next) => {
+    verifyToken(req,res, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+        }else{
+            res.status(500).json("You are not allowed to perform this operation!!!");
+        }
+    })
+}
